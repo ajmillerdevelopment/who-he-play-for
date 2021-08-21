905a473cd8664827dbf18757e04305e1a9b6f007
Alex Miller <ajm9594@gmail.com>
2021-08-21 01:24:07 -0700

    answer key

---
+++ src/helpers/answers.js
@@ -0,0 +1,195 @@
+const answers = [
+    {
+        team: 'atl',
+        city: 'atlanta',
+        mascot: 'hawks',
+        full: 'atlanta hawks'
+    },
+    {
+        team: 'phi',
+        city: 'philadelphia',
+        mascot: '76ers',
+        full: 'philadelphia 76ers',
+        alts: ['philly', 'sixers']
+    },
+    {
+        team: 'bkn',
+        city: 'brooklyn',
+        mascot: 'nets',
+        full: 'brooklyn nets'
+    },
+    {
+        team: 'mil',
+        city: 'milwaukee',
+        mascot: 'bucks',
+        full: 'milwaukee bucks'
+    },
+    {
+        team: 'nyk',
+        city: 'new york',
+        mascot: 'knicks',
+        full: 'new york knicks',
+        alts: ['ny']
+    },
+    {
+        team: 'mia',
+        city: 'miami',
+        mascot: 'heat',
+        full: 'miami heat'
+    },
+    {
+        team: 'bos',
+        city: 'boston',
+        mascot: 'celtics',
+        full: 'boston celtics'
+    },
+    {
+        team: 'was',
+        city: 'washington',
+        mascot: 'wizards',
+        full: 'washington wizards',
+        alts: ['dc', 'wiz']
+    },
+    {
+        team: 'ind',
+        city: 'indiana',
+        mascot: 'pacers',
+        full: 'indana pacers'
+    },
+    {
+        team: 'cha',
+        city: 'charlotte',
+        mascot: 'hornets',
+        full: 'charlotte hornets'
+    },
+    {
+        team: 'chi',
+        city: 'chicago',
+        mascot: 'bulls',
+        full: 'chicago bulls'
+    },
+    {
+        team: 'tor',
+        city: 'toronto',
+        mascot: 'raptors',
+        full: 'toronto raptors',
+        alts: ['raps']
+    },
+    {
+        team: 'cle',
+        city: 'cleveland',
+        mascot: 'cavaliers',
+        full: 'cleveland cavaliers',
+        alts: ['cavs']
+    },
+    {
+        team: 'orl',
+        city: 'orlando',
+        mascot: 'magic',
+        full: 'orlando magic'
+    },
+    {
+        team: 'det',
+        city: 'detroit',
+        mascot: 'pistons',
+        full: 'detroit pistons'
+    },
+    {
+        team: 'uta',
+        city: 'utah',
+        mascot: 'jazz',
+        full: 'utah jazz'
+    },
+    {
+        team: 'phx',
+        city: 'phoenix',
+        mascot: 'suns',
+        full: 'phoenix suns'
+    },
+    {
+        team: 'den',
+        city: 'denver',
+        mascot: 'nuggets',
+        full: 'denver nuggets'
+    },
+    {
+        team: 'lac',
+        city: null,
+        mascot: 'clippers',
+        full: 'los angeles clippers',
+        alts: ['clips']
+    },
+    {
+        team: 'dal',
+        city: 'dallas',
+        mascot: 'mavericks',
+        full: 'dallas mavericks',
+        alts: ['mavs']
+    },
+    {
+        team: 'por',
+        city: 'portland',
+        mascot: 'trail blazers',
+        full: 'portland trail blazers',
+        alts: ['blazers', 'trailblazers', 'portland trailblazers']
+    },
+    {
+        team: 'lal',
+        city: null,
+        mascot: 'lakers',
+        full: 'los angeles lakers'
+    },
+    {
+        team: 'mem',
+        city: 'memphis',
+        mascot: 'grizzlies',
+        full: 'memphis grizzlies',
+        alts: ['grizz']
+    },
+    {
+        team: 'gsw',
+        city: 'golden state',
+        mascot: 'warriors',
+        full: 'golden state warriors',
+        alts: ['dubs', 'gs']
+    },
+    {
+        team: 'sas',
+        city: 'san antonio',
+        mascot: 'spurs',
+        full: 'san antonio spurs'
+    },
+    {
+        team: 'nop',
+        city: 'new orleans',
+        mascot: 'pelicans',
+        full: 'new orleans pelicans',
+        alts: ['no', 'nola', 'pellies']
+    },
+    {
+        team: 'sac',
+        city: 'sacramento',
+        mascot: 'kings',
+        full: 'sacramento kings'
+    },
+    {
+        team: 'min',
+        city: 'minnesota',
+        mascot: 'timberwolves',
+        full: 'minnesota timberwolves',
+        alts: ['minny', 'wolves']
+    },
+    {
+        team: 'okc',
+        city: 'oklahoma city',
+        mascot: 'thunder',
+        full: 'oklahoma city thunder'
+    },
+    {
+        team: 'hou',
+        city: 'houston',
+        mascot: 'rockets',
+        full: 'houston rockets'
+    },
+]
+export default answers
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import data from '../helpers/data'
 import getRandomInt from "../helpers/getRandomInt"
+import answers from '../helpers/answers'
 // markup
 const IndexPage = () => {
   const init = getRandomInt(0, (data.length + 1))
